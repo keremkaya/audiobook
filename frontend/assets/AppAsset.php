905ac577b15de7325285d15fe32a5ef7d0f7bb5b
ibrahim aşık <asikibrahim93@gmail.com>
2016-04-19 23:45:30 +0300

    layoutlar düzenlendi

--- frontend/assets/AppAsset.php
+++ frontend/assets/AppAsset.php
@@ -18,12 +18,35 @@ class AppAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-        'css/site.css',
+	    'css/assets/plugins/font-awesome/css/font-awesome.min.css',
+	    'css/assets/plugins/bootstrap/css/bootstrap.min.css',
+	    'css/assets/pages/css/animate.css',
+	    'css/assets/plugins/fancybox/source/jquery.fancybox.css',
+	    'css/assets/owl.carousel.css',
+	    'css/assets/pages/css/components.css',
+	    'css/assets/pages/css/components.css',
+	    'css/assets/pages/css/slider.css',
+	    'css/assets/pages/css/style-shop.css',
+	    'css/assets/corporate/css/style.css',
+	    'css/assets/corporate/css/style-responsive.css',
+	    'css/assets/corporate/css/themes/red.css',
+	    'css/assets/corporate/css/custom.css',
     ];
     public $js = [
+	    'css/assets/plugins/jquery.min.js', 
+	    'css/assets/plugins/jquery-migrate.min.js', 
+	    'css/assets/plugins/bootstrap/js/bootstrap.min.js' ,
+	    'css/assets/corporate/scripts/back-to-top.js', 
+	    'css/assets/plugins/jquery-slimscroll/jquery.slimscroll.min.js' ,
+	    'css/assets/plugins/fancybox/source/jquery.fancybox.pack.js' ,
+	    'css/assets/owl.carousel.min.js' ,
+	    'css/assets/plugins/zoom/jquery.zoom.min.js' ,
+	    'css/assets/plugins/bootstrap-touchspin/bootstrap.touchspin.js',
+	    'css/assets/corporate/scripts/layout.js' ,
+	    'css/assets/pages/scripts/bs-carousel.js',
     ];
     public $depends = [
         'yii\web\YiiAsset',
-        'yii\bootstrap\BootstrapAsset',
+        //'yii\bootstrap\BootstrapAsset',
     ];
 }
